f5b5620ec186e31e59b6848f3a722016b7a94a9e
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-11-08 09:22:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ console.log(removeChar('ooopsss'));
 console.log(removeChar('country'));
 
 // KATA 2 7kyu
-const maskify = numstr => numstr.slice(0, -4).replace(/./g, '#') + numstr.slice(-4)
+const maskify = numstr => numstr.slice(0, -4).replace(/./g, '#') + numstr.slice(-4);
 
 console.log(maskify('4556364607935616'));
 console.log(maskify('64607935616'));
 console.log(maskify('1'));
 console.log(maskify(''));
-
-
 
 // KATA 3 6kyu
 const isPanagram = str => str.match((/([a-z])(?!.*\1)/gi) || []).length === 26;
