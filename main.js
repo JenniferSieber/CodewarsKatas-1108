12705eb5b3b58f56676cbbae162223bdd15b1a53
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-10-24 07:58:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // Strings & Arrays only
+// October 15, 2024
+const sumMix = array => array.reduce((a,b) => Number(a) + Number(b), 0);
+console.log(sumMix([9, 3, '7', '3'])); // 22
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7])); // 42
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])); // 41
+
 // October 4, 2024
 function domainName(string) {
   let stringArray;
